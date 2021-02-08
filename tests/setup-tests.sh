f600df60a9da37eb0e80a45319d67d510588876d
Tudor <sekaru@hotmail.co.uk>
2021-02-08 19:53:17 +0000

    use different docker project for testing

--- tests/setup-tests.sh
+++ tests/setup-tests.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 mv .env .env.backup
 cp envs/.env.test .env
+
+docker-compose -f docker-compose.test.yml -p gs-test up -d
+
 env TS_NODE_COMPILER_OPTIONS='{"module": "commonjs" }' mocha -r ts-node/register 'tests/**/*.ts'
+
 rm .env
 mv .env.backup .env
+
+docker-compose -p gs-test down

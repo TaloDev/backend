08a470c137643cef758c168495f264eaa6b6051a
Tudor <sekaru@sleepystudios.net>
2021-02-11 23:04:07 +0000

    schema scrip checks for docker output to check if db is ready

--- tests/create-schema.ts
+++ tests/create-schema.ts
@@ -1,31 +1,35 @@
 import 'dotenv/config'
 import { MikroORM } from '@mikro-orm/core'
+import { exec } from 'child_process'
 
 let interval: NodeJS.Timeout
+let lastOutput: string = 'Waiting for DB to be ready '
 
-const done = () => {
+const done = (): void => {
   clearInterval(interval)
 }
 
-const createSchema = async () => {
+const createSchema = async (): Promise<void> => {
   try {
     const orm = await MikroORM.init()
     const generator = orm.getSchemaGenerator()
     await generator.createSchema()
     await orm.close(true)
   } catch (err) {
-    console.log(err.code)
-    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-      console.error(err)
-      done()
-    }
+    console.error(err)
+    process.exit(1)
   }
 }
 
-interval = setInterval(async () => {
-  console.log('Checking DB connection...')
-  await createSchema()
-  console.log('Schema created')
-  done()
-}, 2000)
 
+interval = setInterval(async () => {
+  lastOutput = `${lastOutput}.`
+  process.stdout.write(`${lastOutput}\r`)
+
+  exec('docker logs gs-test_db_1 -n 1', async (err, stdout, stderr) => {
+    if (stderr.includes('[MY-010931]')) {
+      done()
+      await createSchema()
+    }
+  })
+}, 1000)

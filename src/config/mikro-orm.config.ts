f3b0fa3ff912720e16ce9e4f282c489981c7bce5
Tudor <sekaru@sleepystudios.net>
2021-05-05 23:37:59 +0100

    look in correct places for entities in prod

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -2,14 +2,15 @@ import 'dotenv/config'
 import { Options } from '@mikro-orm/core'
 
 const opts: Options = {
-  entities: ['./dist/entities/*.js'],
+  entities: ['./entities/*.js'],
   entitiesTs: ['./src/entities/*.ts'],
   type: 'mysql',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   dbName: process.env.MYSQL_DATABASE,
   user: process.env.DB_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  debug: true
 }
 
 export default opts

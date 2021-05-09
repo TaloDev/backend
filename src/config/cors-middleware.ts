1d1e377c3e83c73fcb84eebd80b0abc1f9308653
Tudor <sekaru@sleepystudios.net>
2021-05-09 12:02:45 +0100

    rename api to v1

--- src/config/cors-middleware.ts
+++ src/config/cors-middleware.ts
@@ -2,7 +2,7 @@ import { Context, Next } from 'koa'
 import cors from '@koa/cors'
 
 export default async (ctx: Context, next: Next): Promise<void> => {
-  if (ctx.path.match(/^\/(api)\//)) {
+  if (ctx.path.match(/^\/(v1)\//)) {
     await next()
   } else {
     return cors({ 

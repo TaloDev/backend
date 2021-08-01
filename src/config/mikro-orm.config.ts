ea2fa9d3a2369ac63e93ec7cfbdc67a3bfd0404e
Tudor <sekaru@sleepystudios.net>
2021-08-01 20:08:18 +0100

    put migrations in src folder

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config'
 import { Options } from '@mikro-orm/core'
 import entities from '../entities'
+import migrationsList from '../migrations'
 
 const ormConfig: Options = {
   entities,
@@ -9,7 +9,11 @@ const ormConfig: Options = {
   port: Number(process.env.DB_PORT),
   dbName: process.env.MYSQL_DATABASE,
   user: process.env.DB_USER,
-  password: process.env.MYSQL_ROOT_PASSWORD
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  migrations: {
+    migrationsList,
+    path: 'src/migrations' // for generating migrations via the cli
+  }
 }
 
 export default ormConfig
---
+++ src/migrations/index.ts
@@ -0,0 +1,8 @@
+import { Migration20210725211129 } from './Migration20210725211129'
+
+export default [
+  {
+    name: 'Migration20210725211129',
+    class: Migration20210725211129
+  }
+]

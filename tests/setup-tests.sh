99c37eace5b1e7882ae92036465f4aeabeb7507c
Tudor <sekaru@sleepystudios.net>
2021-08-06 10:14:01 +0100

    remove testing database, use normal database and backup/restore

--- tests/setup-tests.sh
+++ tests/setup-tests.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
+export $(grep -e DB_NAME -e DB_PASS .env | xargs)
+export DB_HOST=127.0.0.1
+
 trap 'cleanup' EXIT
 
 cleanup() {
   if [ -z "$CI" ]
   then
-    rm .env
-    [ -f .env.backup ] && mv .env.backup .env
+    cat backup.sql | yarn dc exec -i db /usr/bin/mysql --password=$DB_PASS
+    rm -rf backup.sql
   fi
 }
 
 set -e
 
-[ -f .env ] && mv .env .env.backup
-cp envs/.env.test .env
+if [ -z "$CI" ]
+then
+  yarn dc exec db /usr/bin/mysqldump $DB_NAME --password=$DB_PASS > backup.sql
+fi
 
 ./node_modules/.bin/ts-node tests/refresh-db.ts 
-./node_modules/.bin/jest "$@" --runInBand
+node --trace-warnings ./node_modules/.bin/jest "$@" --runInBand

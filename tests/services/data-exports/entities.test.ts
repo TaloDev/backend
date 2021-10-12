07f9a7c654c75e66376c103d4d8dc251b68bfb8f
Tudor <7089284+tudddorrr@users.noreply.github.com>
2021-10-12 20:11:17 +0100

    fix test

--- tests/services/data-exports/entities.test.ts
+++ tests/services/data-exports/entities.test.ts
@@ -5,14 +5,12 @@ import request from 'supertest'
 import User from '../../../src/entities/user'
 import { genAccessToken } from '../../../src/lib/auth/buildTokenPair'
 import UserFactory from '../../fixtures/UserFactory'
-import Game from '../../../src/entities/game'
 
 const baseUrl = '/data-exports'
 
 describe('Data exports service - available entities', () => {
   let app: Koa
   let user: User
-  let game: Game
   let token: string
 
   beforeAll(async () => {
@@ -31,10 +29,9 @@ describe('Data exports service - available entities', () => {
   it('should return a list of available data export entities', async () => {
     const res = await request(app.callback())
       .get(`${baseUrl}/entities`)
-      .query({ gameId: game.id })
       .auth(token, { type: 'bearer' })
       .expect(200)
 
-    expect(res.body.entities).toStrictEqual([ 'players', 'events', 'playerAliases' ])
+    expect(res.body.entities).toStrictEqual([ 'events', 'players', 'playerAliases' ])
   })
 })

c0dbbf4e1f64b51ef13cc7a91f49985ee049d71b
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-10-03 21:07:02 +0100

    fix test

--- tests/services/player-group/indexPinned.test.ts
+++ tests/services/player-group/indexPinned.test.ts
@@ -3,13 +3,18 @@ import request from 'supertest'
 import createOrganisationAndGame from '../../utils/createOrganisationAndGame'
 import createUserAndToken from '../../utils/createUserAndToken'
 import UserPinnedGroupFactory from '../../fixtures/UserPinnedGroupFactory'
+import PlayerGroupFactory from '../../fixtures/PlayerGroupFactory'
 
 describe('Player group service - index pinned', () => {
   it('should return a list of groups', async () => {
     const [organisation, game] = await createOrganisationAndGame()
     const [token, user] = await createUserAndToken({}, organisation)
 
-    const pinned = await new UserPinnedGroupFactory().state(() => ({ user })).many(3)
+    const pinned = await new UserPinnedGroupFactory()
+      .state(() => ({ user }))
+      .state(async () => ({ group: await new PlayerGroupFactory().construct(game).one() }))
+      .many(3)
+
     await (<EntityManager>global.em).persistAndFlush(pinned)
 
     const res = await request(global.app)

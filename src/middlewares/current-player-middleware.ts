179f4ae699d4ce81f9423d2ef849c20ca98b23cb
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-12-09 09:26:52 +0000

    identify for talo aliases must be called with a session token

--- src/middlewares/current-player-middleware.ts
+++ src/middlewares/current-player-middleware.ts
@@ -1,8 +1,15 @@
 import { Context, Next } from 'koa'
 
+export function setCurrentPlayerState(ctx: Context, playerId: string, aliasId: number) {
+  ctx.state.currentPlayerId = playerId
+  ctx.state.currentAliasId = aliasId
+}
+
 export default async (ctx: Context, next: Next): Promise<void> => {
-  ctx.state.currentPlayerId = ctx.headers['x-talo-player']
-  ctx.state.currentAliasId = ctx.headers['x-talo-alias'] ? Number(ctx.headers['x-talo-alias']) : undefined
+  setCurrentPlayerState(
+    ctx, ctx.headers['x-talo-player'] as string,
+    ctx.headers['x-talo-alias'] ? Number(ctx.headers['x-talo-alias']) : undefined
+  )
 
   await next()
 }

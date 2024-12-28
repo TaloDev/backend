aa0b147cfa3fb6f190a1c324482e2d54becbd821
tudor <7089284+tudddorrr@users.noreply.github.com>
2024-12-28 22:16:21 +0000

    socket tickets api docs

---
+++ src/docs/socket-tickets-api.docs.ts
@@ -0,0 +1,18 @@
+import SocketTicketAPIService from '../services/api/socket-ticket-api.service'
+import APIDocs from './api-docs'
+
+const SocketTicketsAPIDocs: APIDocs<SocketTicketAPIService> = {
+  post: {
+    description: 'Create a socket ticket (expires after 5 minutes)',
+    samples: [
+      {
+        title: 'Sample response',
+        sample: {
+          ticket: '6c6ef345-0ac3-4edc-a221-b807fbbac4ac'
+        }
+      }
+    ]
+  }
+}
+
+export default SocketTicketsAPIDocs
--- src/services/api/socket-ticket-api.service.ts
+++ src/services/api/socket-ticket-api.service.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from 'koa-clay'
+import { Docs, Request, Response } from 'koa-clay'
 import APIService from './api-service'
 import { createRedisConnection } from '../../config/redis.config'
 import { v4 } from 'uuid'
 import Redis from 'ioredis'
 import APIKey from '../../entities/api-key'
+import SocketTicketsAPIDocs from '../../docs/socket-tickets-api.docs'
 
 export async function createSocketTicket(redis: Redis, key: APIKey, devBuild: boolean): Promise<string> {
   const ticket = v4()
@@ -14,7 +15,8 @@ export async function createSocketTicket(redis: Redis, key: APIKey, devBuild: bo
 }
 
 export default class SocketTicketAPIService extends APIService {
-  async index(req: Request): Promise<Response> {
+  @Docs(SocketTicketsAPIDocs.post)
+  async post(req: Request): Promise<Response> {
     const redis = createRedisConnection(req.ctx)
 
     const ticket = await createSocketTicket(redis, req.ctx.state.key, req.headers['x-talo-dev-build'] === '1')

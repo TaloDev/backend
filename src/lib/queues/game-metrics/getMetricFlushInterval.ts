a64367fd75369f6a22b4451dd7d103eaa51f6aaa
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-07-05 07:17:19 +0100

    flush every 30 seconds

--- src/lib/queues/game-metrics/getMetricFlushInterval.ts
+++ src/lib/queues/game-metrics/getMetricFlushInterval.ts
@@ -1,7 +1,7 @@
 export function getMetricFlushInterval() {
   const flushInterval = process.env.GAME_METRICS_FLUSH_INTERVAL
     ? Number(process.env.GAME_METRICS_FLUSH_INTERVAL)
-    : 10_000
+    : 30_000
 
   return flushInterval
 }

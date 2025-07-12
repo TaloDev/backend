5fbcb3a55508aeeb71112ea9d6dd9d12365e2cea
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-07-12 20:49:23 +0100

    smarter rate limiter

--- src/middleware/limiter-middleware.ts
+++ src/middleware/limiter-middleware.ts
@@ -2,18 +2,23 @@ import { Context, Next } from 'koa'
 import { isAPIRoute } from './route-middleware'
 import checkRateLimitExceeded from '../lib/errors/checkRateLimitExceeded'
 
-const DEFAULT_MAX_REQUESTS = 50
-const rateLimitOverrides = new Map<string, number>([
-  ['/v1/players/auth', 5],
-  ['/v1/players/identify', 5],
-  ['/v1/socket-tickets', 5]
+const limitMap = {
+  default: 50,
+  auth: 5
+} as const
+
+const rateLimitOverrides = new Map<string, keyof typeof limitMap>([
+  ['/v1/players/auth', 'auth'],
+  ['/v1/players/identify', 'auth'],
+  ['/v1/socket-tickets', 'auth']
 ])
 
 export default async function limiterMiddleware(ctx: Context, next: Next): Promise<void> {
   if (isAPIRoute(ctx) && process.env.NODE_ENV !== 'test') {
-    const key = ctx.state.user.sub
-    const maxRequests = rateLimitOverrides.get(ctx.request.path) ?? DEFAULT_MAX_REQUESTS
-    if (await checkRateLimitExceeded(ctx.redis, key, maxRequests)) {
+    const limitMapKey = rateLimitOverrides.get(ctx.request.path) ?? 'default'
+    const redisKey = `requests:${ctx.state.user.sub}:${ctx.request.ip}:${limitMapKey}`
+
+    if (await checkRateLimitExceeded(ctx.redis, redisKey, limitMap[limitMapKey])) {
       ctx.throw(429)
     }
   }

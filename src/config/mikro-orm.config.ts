5e0aec67ba278fe8017a89158e2e5d80f15cc92a
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-08-04 17:42:49 +0100

    improve testing db pooling setup

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -23,8 +23,10 @@ export default defineConfig({
   metadataProvider: TsMorphMetadataProvider,
   extensions: [Migrator],
   pool: {
-    min: Number(process.env.MIKRO_ORM_POOL_MIN) || 2,
-    max: Number(process.env.MIKRO_ORM_POOL_MAX) || 10
+    min: Number(process.env.MIKRO_ORM_POOL_MIN) || 0,
+    max: Number(process.env.MIKRO_ORM_POOL_MAX) || 10,
+    idleTimeoutMillis: Number(process.env.MIKRO_ORM_POOL_IDLE_TIMEOUT) || undefined,
+    acquireTimeoutMillis: Number(process.env.MIKRO_ORM_POOL_ACQUIRE_TIMEOUT) || undefined
   },
   resultCache: {
     adapter: RedisCacheAdapter,

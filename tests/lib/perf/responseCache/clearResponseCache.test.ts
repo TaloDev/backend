995e8243615a206ddf4ce572176be64425ed1e87
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-09-14 21:10:31 +0100

    better response cache clearing

--- tests/lib/perf/responseCache/clearResponseCache.test.ts
+++ tests/lib/perf/responseCache/clearResponseCache.test.ts
@@ -1,6 +1,11 @@
+import Redis from 'ioredis'
 import { clearResponseCache, prefix } from '../../../../src/lib/perf/responseCache'
 
 describe('clearResponseCache', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
   it('should delete specific keys', async () => {
     await redis.set(`${prefix}:key1`, 'key1')
     await redis.set(`${prefix}:key2`, 'key2')
@@ -19,5 +24,15 @@ describe('clearResponseCache', () => {
     expect(await redis.get(`${prefix}:key1`)).toBe(null)
     expect(await redis.get(`${prefix}:key2`)).toBe(null)
     expect(await redis.get(`${prefix}:other`)).toBe('other')
+  })
+
+  it('should catch redis errors', async () => {
+    const evalSpy = vi.spyOn(Redis.prototype, 'eval').mockRejectedValueOnce(new Error())
+    await redis.set(`${prefix}:key1`, 'key1')
+    await redis.set(`${prefix}:key2`, 'key2')
+
+    const res = await clearResponseCache(redis, 'key*')
+    expect(res).toBe(0)
+    expect(evalSpy).toHaveBeenCalledTimes(1)
   })
 })

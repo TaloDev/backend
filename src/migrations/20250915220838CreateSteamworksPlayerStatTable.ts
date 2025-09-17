bd8ea71f1bbb55eabb59433ac082f74535cc477a
tudor <7089284+tudddorrr@users.noreply.github.com>
2025-09-17 20:35:25 +0100

    add backfill for steamworks_player_stat

--- src/migrations/20250915220838CreateSteamworksPlayerStatTable.ts
+++ src/migrations/20250915220838CreateSteamworksPlayerStatTable.ts
@@ -9,6 +9,27 @@ export class CreateSteamworksPlayerStatTable extends Migration {
 
     this.addSql('alter table `steamworks_player_stat` add constraint `steamworks_player_stat_stat_id_foreign` foreign key (`stat_id`) references `game_stat` (`id`) on update cascade on delete cascade;')
     this.addSql('alter table `steamworks_player_stat` add constraint `steamworks_player_stat_player_stat_id_foreign` foreign key (`player_stat_id`) references `player_game_stat` (`id`) on update cascade on delete set null;')
+
+    this.addSql(`
+      insert into steamworks_player_stat (
+        stat_id,
+        player_stat_id,
+        steam_user_id,
+        created_at,
+        updated_at
+      ) 
+      select
+        pgs.stat_id,
+        pgs.id,
+        pa.identifier,
+        pgs.created_at,
+        pgs.updated_at
+      from player_game_stat pgs
+        inner join game_stat gs on gs.id = pgs.stat_id
+        inner join integration i on i.game_id = gs.game_id and i.type = 'steamworks'
+        inner join player p on p.id = pgs.player_id
+        inner join player_alias pa on pa.player_id = p.id and pa.service = 'steam'
+    `)
   }
 
   override async down(): Promise<void> {

90e0d7361659b71a88ac94ad43de1b099f18889f
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-01-10 22:48:38 +0000

    simplify queue config

--- src/config/global-queues.ts
+++ src/config/global-queues.ts
@@ -1,32 +1,29 @@
 import { Queue } from 'bullmq'
-import createEmailQueue from '../lib/queues/createEmailQueue'
+import { createEmailQueue } from '../lib/queues/createEmailQueue'
 import { createClearResponseCacheQueue } from '../lib/perf/responseCacheQueue'
-import { EmailConfig } from '../emails/mail'
-import { createDeleteClickHousePlayerDataQueue, DeleteClickHousePlayerDataConfig } from '../lib/queues/createDeleteClickHousePlayerDataQueue'
-import { createDemoUserQueue, DemoUserConfig } from '../lib/queues/createDemoUserQueue'
+import { createDeleteClickHousePlayerDataQueue } from '../lib/queues/createDeleteClickHousePlayerDataQueue'
+import { createDemoUserQueue } from '../lib/queues/createDemoUserQueue'
 
-export const queueNames = [
-  'email',
-  'clear-response-cache',
-  'delete-clickhouse-player-data',
-  'demo'
-] as const
+const queueFactories = {
+  'email': createEmailQueue,
+  'clear-response-cache': createClearResponseCacheQueue,
+  'delete-clickhouse-player-data': createDeleteClickHousePlayerDataQueue,
+  'demo': createDemoUserQueue
+} as const
+
+export const queueNames = Object.keys(queueFactories) as (keyof typeof queueFactories)[]
 
 type QueueName = typeof queueNames[number]
 type QueueTypeMapping = {
-  'email': Queue<EmailConfig>
-  'clear-response-cache': Queue<string>
-  'delete-clickhouse-player-data': Queue<DeleteClickHousePlayerDataConfig>
-  'demo': Queue<DemoUserConfig>
+  [K in keyof typeof queueFactories]: ReturnType<typeof queueFactories[K]>
 }
 
 const queueMap = new Map<QueueName, Queue>()
 
 export function setupGlobalQueues() {
-  queueMap.set('email', createEmailQueue())
-  queueMap.set('clear-response-cache', createClearResponseCacheQueue())
-  queueMap.set('delete-clickhouse-player-data', createDeleteClickHousePlayerDataQueue())
-  queueMap.set('demo', createDemoUserQueue())
+  for (const [name, factory] of Object.entries(queueFactories)) {
+    queueMap.set(name as QueueName, factory())
+  }
 }
 
 export function getGlobalQueue<T extends QueueName>(queueName: T): QueueTypeMapping[T] {
--- src/lib/queues/createEmailQueue.ts
+++ src/lib/queues/createEmailQueue.ts
@@ -2,12 +2,10 @@ import createQueue, { WorkerEvents } from './createQueue'
 import sendEmail from '../messaging/sendEmail'
 import { EmailConfig } from '../../emails/mail'
 
-const createEmailQueue = (events: WorkerEvents<EmailConfig> = {}, prefix = '') => {
+export function createEmailQueue(events: WorkerEvents<EmailConfig> = {}, prefix = '') {
   const queue = createQueue<EmailConfig>(prefix + 'email', async (job) => {
     await sendEmail(job.data.mail)
   }, events)
 
   return queue
 }
-
-export default createEmailQueue

ee60f0aaa5ea945b17d59224f5d353e1bf963e60
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-02-18 22:12:45 +0000

    add stats global value chart

--- src/routes/protected/chart/index.ts
+++ src/routes/protected/chart/index.ts
@@ -2,11 +2,13 @@ import { protectedRouter } from '../../../lib/routing/router'
 import { newLeaderboardEntriesRoute } from './new-leaderboard-entries'
 import { newPlayersRoute } from './new-players'
 import { statsActivityRoute } from './stats-activity'
+import { statsGlobalValueRoute } from './stats-global-value'
 
 export function chartRouter() {
   return protectedRouter('/games/:gameId/charts', ({ route }) => {
     route(newLeaderboardEntriesRoute)
     route(newPlayersRoute)
     route(statsActivityRoute)
+    route(statsGlobalValueRoute)
   })
 }

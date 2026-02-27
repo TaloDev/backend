cec851dd22e18b722a356b27f4ca533c2a523e76
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-02-27 20:14:06 +0000

    remove unnecessary checks in middleware

--- src/middleware/api-route-middleware.ts
+++ src/middleware/api-route-middleware.ts
@@ -5,6 +5,7 @@ import { isAPIRoute } from '../lib/routing/route-info'
 export async function apiRouteAuthMiddleware(ctx: Context, next: Next) {
   if (isAPIRoute(ctx)) {
     return jwt({
+      // undefined secrets (aka missing api keys) throw a 401
       secret: ctx.state.secret,
       isRevoked: async (ctx) => ctx.state.key.revokedAt !== null,
       key: 'jwt',

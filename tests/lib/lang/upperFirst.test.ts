7119fc6e6a42abb37c3c49cb3f58489a706f62d8
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-04-01 21:28:55 +0100

    fix integration type in game activity

--- tests/lib/lang/upperFirst.test.ts
+++
@@ -1,7 +0,0 @@
-import upperFirst from '../../../src/lib/lang/upperFirst'
-
-describe('upperFirst', () => {
-  it('should uppercase the first character in a string', () => {
-    expect(upperFirst('steamworks')).toBe('Steamworks')
-  })
-})

4f00b65f8de2fed048bc6630cf4dba3876ddd1c3
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-06-13 00:08:19 +0100

    hoist cors middleware to module scope

--- src/middleware/cors-middleware.ts
+++ src/middleware/cors-middleware.ts
@@ -2,13 +2,16 @@ import cors from '@koa/cors'
 import { Context, Next } from 'koa'
 import { isAPIRoute, isPublicHealthCheck } from '../lib/routing/route-info.js'
 
+const apiCors = cors()
+const dashboardCors = cors({
+  credentials: true,
+  origin: process.env.DASHBOARD_URL,
+})
+
 export async function corsMiddleware(ctx: Context, next: Next): Promise<void> {
   if (isPublicHealthCheck(ctx) || isAPIRoute(ctx)) {
-    return cors()(ctx, next)
+    return apiCors(ctx, next)
   } else {
-    return cors({
-      credentials: true,
-      origin: process.env.DASHBOARD_URL,
-    })(ctx, next)
+    return dashboardCors(ctx, next)
   }
 }

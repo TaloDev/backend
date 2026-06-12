ff01181650ee40427b497049b5c5db8b8816ac20
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-06-13 00:06:47 +0100

    trigger integration cache

--- src/lib/integrations/triggerIntegrations.ts
+++ src/lib/integrations/triggerIntegrations.ts
@@ -1,12 +1,16 @@
 import { EntityManager } from '@mikro-orm/mysql'
 import Game from '../../entities/game.js'
 import Integration from '../../entities/integration.js'
+import { getResultCacheOptions } from '../perf/getResultCacheOptions.js'
 
 export default async function triggerIntegrations(
   em: EntityManager,
   game: Game,
-  callback: (integration: Integration) => void,
+  callback: (integration: Integration) => void | Promise<void>,
 ) {
-  const integrations = await em.repo(Integration).find({ game })
-  await Promise.all(integrations.map((integration) => callback(integration)))
+  const integrations = await em
+    .repo(Integration)
+    .find({ game }, getResultCacheOptions(`integrations-${game.id}`))
+
+  await Promise.all(integrations.map(async (integration) => await callback(integration)))
 }

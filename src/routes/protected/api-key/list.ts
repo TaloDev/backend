cf9112f3bfb55c1d86b9b79d18447308a662e68b
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-07-16 00:14:50 +0100

    return last 4 characters of game api keys in list route

--- src/routes/protected/api-key/list.ts
+++ src/routes/protected/api-key/list.ts
@@ -1,6 +1,7 @@
 import APIKey from '../../../entities/api-key.js'
 import { protectedRoute, withMiddleware } from '../../../lib/routing/router.js'
 import { loadGame } from '../../../middleware/game-middleware.js'
+import { createToken } from './common.js'
 
 export const listRoute = protectedRoute({
   method: 'get',
@@ -11,10 +12,17 @@ export const listRoute = protectedRoute({
       .repo(APIKey)
       .find({ game: ctx.state.game, revokedAt: null }, { populate: ['createdByUser'] })
 
+    const apiKeysWithEnding = await Promise.all(
+      apiKeys.map(async (apiKey) => {
+        const token = await createToken(em, apiKey)
+        return { ...apiKey.toJSON(), keyEnding: token.slice(-4) }
+      }),
+    )
+
     return {
       status: 200,
       body: {
-        apiKeys,
+        apiKeys: apiKeysWithEnding,
       },
     }
   },

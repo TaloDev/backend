6c4447dc3b865c90c1e476ee9fa855df2100f358
tudor <7089284+tudddorrr@users.noreply.github.com>
2026-08-17 19:06:17 +0100

    add admin api route for listing leaderboards

--- src/routes/admin/leaderboard/index.ts
+++ src/routes/admin/leaderboard/index.ts
@@ -1,12 +1,14 @@
 import type Router from 'koa-tree-router'
 import { adminRouter } from '../../../lib/routing/router.js'
 import { createLeaderboardAdminRoute } from './create.js'
+import { listLeaderboardsAdminRoute } from './list.js'
 
 export function leaderboardAdminRouter(router: Router) {
   adminRouter(
     '/admin/v1/leaderboards',
     ({ route }) => {
       route(createLeaderboardAdminRoute)
+      route(listLeaderboardsAdminRoute)
     },
     { router, docsKey: 'LeaderboardAdminAPI' },
   )
